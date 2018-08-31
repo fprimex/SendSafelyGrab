0bcb955ae93581b586454b5095f78c24aa1e965f
Brent Woodruff <brent@fprimex.com>
2018-08-31 17:36:43 -0400

    plugging away

--- SendSafelyGrab/Program.cs
+++ SendSafelyGrab/Program.cs
@@ -22,6 +22,10 @@ namespace SendSafelyGrab
                 HelpText = "SendSafely API secret.")]
         public string SendSafelySecret { get; set; }
 
+        [Option('d', "dir", Required = false,
+                HelpText = "Download directory.")]
+        public string DownloadDir { get; set; }
+
         [Option('v', "verbose", Required = false,
                 Default = false,
                 HelpText = "Show verbose output.")]
@@ -41,13 +45,13 @@ namespace SendSafelyGrab
         }
     }
 
-	class VerboseProgressCallback : ISendSafelyProgress
-	{
-		public void UpdateProgress(string prefix, double progress)
-		{
-			Console.Error.WriteLine(prefix + " " + progress + "%");
-		}
-	}
+    class VerboseProgressCallback : ISendSafelyProgress
+    {
+        public void UpdateProgress(string prefix, double progress)
+        {
+            Console.Error.WriteLine(prefix + " " + progress + "%");
+        }
+    }
 
 
     class Program
@@ -69,83 +73,100 @@ namespace SendSafelyGrab
 
         static int RunOptionsAndReturnExitCode(Options options)
         {
+            ClientAPI ssApi = new ClientAPI();
+            string userEmail = "";
+
             string sendSafelyHost = options.SendSafelyHost ?? Environment.GetEnvironmentVariable("SS_HOST");
             string userApiKey = options.SendSafelyKey ?? Environment.GetEnvironmentVariable("SS_KEY");
             string userApiSecret = options.SendSafelySecret ?? Environment.GetEnvironmentVariable("SS_SECRET");
+            string downloadDir = options.DownloadDir ?? Environment.GetEnvironmentVariable("SS_DIR");
             bool verbose = options.Verbose;
             string textLinks = options.TextLinks;
-            string packageId = "";
+            string destFile;
+            System.IO.FileInfo downloadedFile;
 
             if (sendSafelyHost == null || userApiKey == null || userApiSecret == null)
             {
                 // TODO: figure out how to print usage
+                System.Environment.Exit(1);
                 return 1;
             }
 
-            // Initialize the API 
-            VerboseWriteLine(verbose, "Initializing API");
-            ClientAPI ssApi = new ClientAPI();
-            ssApi.InitialSetup(sendSafelyHost, userApiKey, userApiSecret);
-
             try
             {
-                // Verify the API key and Secret before continuing.  
-                // Print the authenticated user's email address to the screen if valid. 
-                string userEmail = ssApi.VerifyCredentials();
-                VerboseWriteLine(verbose, "Connected to SendSafely as user " + userEmail);
+                // Initialize the API 
+                ssApi.InitialSetup(sendSafelyHost, userApiKey, userApiSecret);
 
                 System.Collections.Generic.List<string> links = ssApi.ParseLinksFromText(textLinks);
+
+                if(links.Count == 0)
+                {
+                    return 0;
+                }
+
                 foreach (string plink in links)
                 {
-                    Console.WriteLine(plink);
+                    if(ssApi == null)
+                    {
+                        // Verify the API key and Secret before continuing.  
+                        // Print the authenticated user's email address to the screen if valid. 
+                        userEmail = ssApi.VerifyCredentials();
+
+                        //VerboseWriteLine(verbose, "Connected to SendSafely as user " + userEmail);
+                    }
+
                     PackageInformation pkgToDownload = ssApi.GetPackageInformationFromLink(plink);
                     foreach (SendSafely.File file in pkgToDownload.Files)
                     {
-                        if(! System.IO.File.Exists(file.FileName))
+                        if(! System.IO.Directory.Exists(downloadDir))
                         {
-                            System.IO.FileInfo downloadedFile;
+                            System.IO.Directory.CreateDirectory(downloadDir);
+                        }
+
+                        destFile = Path.GetFullPath(Path.Combine(downloadDir, file.FileName));
+
+                        if(System.IO.File.Exists(destFile))
+                        {
+                            VerboseWriteLine(verbose, " SendSafely " + file.FileName + " already present");
+                        }
+                        else
+                        {
                             if(verbose)
                             {
-                                downloadedFile = ssApi.DownloadFile(pkgToDownload.PackageId, file.FileId, pkgToDownload.KeyCode, new VerboseProgressCallback());
+                                VerboseWriteLine(verbose, " Downloading SendSafely " + file.FileName);
+                                downloadedFile = ssApi.DownloadFile(pkgToDownload.PackageId,
+                                    file.FileId, pkgToDownload.KeyCode,
+                                    new SilentProgressCallback());
+                                    // TODO: make this output pretty and not stupid
+                                    //new VerboseProgressCallback());
                             }
                             else
                             {
-                                downloadedFile = ssApi.DownloadFile(pkgToDownload.PackageId, file.FileId, pkgToDownload.KeyCode, new SilentProgressCallback());
+                                downloadedFile = ssApi.DownloadFile(pkgToDownload.PackageId,
+                                    file.FileId, pkgToDownload.KeyCode,
+                                    new SilentProgressCallback());
                             }
 
-                            VerboseWriteLine(verbose, "Downloaded File to path: " + downloadedFile.FullName);
                             // throws System.IO.IOException on ERROR_ALREADY_EXISTS
-                            System.IO.File.Move(downloadedFile.FullName, System.IO.Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + file.FileName);
-                            VerboseWriteLine(verbose, "Moved file to: " + System.IO.Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + file.FileName);
+                            System.IO.File.Move(downloadedFile.FullName, destFile);
+                            //VerboseWriteLine(verbose, "Moved file to: " + destFile);
+                            Console.WriteLine(file.FileName);
                         }
                     }
                 }
             }
             catch (SendSafely.Exceptions.BaseException ex)
             {
-                // Catch any custom SendSafelyAPI exceptions and determine how to properly handle them
-                if (ex is SendSafely.Exceptions.FileUploadException || ex is SendSafely.Exceptions.InvalidEmailException || ex is SendSafely.Exceptions.InvalidPhonenumberException || ex is SendSafely.Exceptions.InvalidRecipientException || ex is SendSafely.Exceptions.PackageFinalizationException || ex is SendSafely.Exceptions.ApproverRequiredException)
-                {
-                    // These exceptions indicate a problem that occurred during package preparation.  
-                    // If a package was created, delete it so it does not remain in the user's incomplete pacakge list.  
-                    VerboseWriteLine(verbose, "Error: " + ex.Message);
-                    if (!String.IsNullOrEmpty(packageId))
-                    {
-                        ssApi.DeleteTempPackage(packageId);
-                        VerboseWriteLine(verbose, "Deleted Package - Id#:" + packageId);
-                    }
-                }
-                else
-                {
-                    // Throw the exception if it was not one of the specific ones we handled by deleting the package. 
-                    VerboseWriteLine(verbose, "Error: " + ex.Message);
-                }
+                Console.Error.WriteLine(" Error: " + ex.Message);
+                System.Environment.Exit(1);
+                return 1;
             }
             return 0;
         }
 
         static int HandleParseError(IEnumerable<CommandLine.Error> errs)
         {
+            System.Environment.Exit(1);
             return 1;
         }
     }
